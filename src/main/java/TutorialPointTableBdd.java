5f895cc4599345f65c70c76297b1d126421fd34f
Jozbo-a11 <jozef.boko@gmail.com>
2025-07-03 19:53:27 +0200

    Finish test and add cucumber file

---
+++ src/main/java/TutorialPointTableBdd.java
@@ -0,0 +1,17 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class TutorialPointTableBdd {
+    @Given("Uzivatel sa nachadza na stranke {string}")
+    public void uzivatelSaNachadzaNaStranke(String arg0) {
+    }
+
+    @Then("Meno na druhom riadku tabulky bude {string}")
+    public void menoNaDruhomRiadkuTabulkyBude(String arg0) {
+    }
+
+    @And("Priezvisko na druhom riadku tabulky bude {string}")
+    public void priezviskoNaDruhomRiadkuTabulkyBude(String arg0) {
+    }
+}
--- src/main/resources/TutorialPointTable.feature
+++ src/main/resources/TutorialPointTable.feature
@@ -1,2 +1,6 @@
 Feature: TutorialPointTable
 
+    Scenario: Overenie dat tabulky
+      Given Uzivatel sa nachadza na stranke "https://www.tutorialspoint.com/selenium/practice/webtables.php"
+      Then Meno na druhom riadku tabulky bude "Alden"
+      And Priezvisko na druhom riadku tabulky bude "Cantrell"
