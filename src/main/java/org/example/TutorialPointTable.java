6fdd0b2bc8b4c68b24639c8883dfbd99c017de54
Jozbo-a11 <jozef.boko@gmail.com>
2025-07-03 19:48:20 +0200

    Finish test and add cucumber file

--- src/main/java/org/example/TutorialPointTable.java
+++ src/main/java/org/example/TutorialPointTable.java
@@ -1,9 +1,14 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TutorialPointTable {
 
@@ -13,6 +18,34 @@ public class TutorialPointTable {
     public void setUp(){
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
+    }
+
+    @AfterEach
+    public void cleanUp(){
+        //driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void test(){
+        driver.get("https://www.tutorialspoint.com/selenium/practice/webtables.php");
+        driver.manage().window().setSize(new Dimension(1936, 1000));
+
+        String name = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[1]")).getText();
+        String lastName = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[2]")).getText();
+        String age = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[3]")).getText();
+        String email = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[4]")).getText();
+        String salary = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[5]")).getText();
+        String department = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[6]")).getText();
+
+        assertEquals("Alden", name);
+        assertEquals("Cantrell", lastName);
+        assertEquals("45", age);
+        assertEquals("alden@example.com", email);
+        assertEquals("12000", salary);
+        assertEquals("Compliance", department);
+
+
     }
 
 
